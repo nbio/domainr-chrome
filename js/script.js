c0d1527b87c772739e653429648d2cf144d0a45b
Ian Gilman <ian@iangilman.com>
2015-09-16 17:23:31 -0700

    Switched over to using the JS SDK

--- js/script.js
+++ js/script.js
@@ -1,106 +1,23 @@
-var t;
-
-$(document).ready( function() {
+(function() {
 
   var createChromeTab = function(url) {
     return chrome.tabs.create({
       url: url
-    })
-  }
-
-  var API_URL = "https://api.domainr.com/v1/search?client_id={your-mashape-key}&q="
-    , selected_domain
-
-  $("#search-form").submit( function(evt) {
-    evt.preventDefault()
-
-    $("#loader").css('visibility', 'visible');
-    var query = $("#query").val()
-
-    if (!query.length) {
-      $("#search-query").remove();
-      $.each($("#results-list li"), function(idx, el) {
-        $(el).remove();
-      });
-      $("#loader").css('visibility', 'hidden');     // hide the spinny thingy.
-    } else {
-      $.getJSON(API_URL + query, null, function(response) {
-        $("#results-list").empty()
-        if ($("#search-query").length) {
-          $("#search-query").text(query)
-        } else {
-          $("<p id='search-query'>" + query + "</p>").insertBefore("#results")
-        }
-        $.each(response.results,function(i, result){
-          $("#results-list").append("<li class='" + result.availability + "'><a href='https://domainr.com/" + query + "'><span class='bg'></span><span class='domain'>" + result.domain + "</span></a></li>")
-        })
-        $("#loader").css('visibility', 'hidden');     // hide the spinny thingy.
-      });
-    }
-  })
-
-  function moveSelectionUp() {
-    if ( $(".selected").length ) {
-      if ( $(".selected").prev().length ) {
-        $("#results-list li.selected").removeClass('selected')
-                                      .prev()
-                                      .addClass('selected')
-      } else {
-        $("#results-list li.selected").removeClass('selected')
-        $("#results-list li").last().addClass('selected')
-      }
-    } else {
-      $("#results-list li").last().addClass('selected')
-    }
+    });
   };
 
-  function moveSelectionDown() {
-    if ( $(".selected").length ) {
-      if ( $(".selected").next().length ) {
-        $("#results-list li.selected").removeClass('selected')
-                                      .next()
-                                      .addClass('selected')
-      } else {
-        $("#results-list li.selected").removeClass('selected')
-        $("#results-list li").first().addClass('selected')
-      }
-    } else {
-      $("#results-list li").first().addClass('selected')
-    }
-  };
+  var _in = document.getElementById('query');
+  var _results = document.getElementById('results');
 
-  $("#query").keydown( function(evt) {
-    var keyCode = evt.keyCode;
-
-    if (keyCode === 38 || keyCode === 40) {
-      if (keyCode === 38) {
-        moveSelectionUp();
-      } else {
-        moveSelectionDown();
-      }
-    }
-
-    else if (keyCode === 13) {
-      var val = $(this).val();
-      var $selected = $(".selected");
-      if ($selected.length) {
-        var url = $(".selected a").attr('href') + "/with/" + $(".selected a").text();
-        createChromeTab(url);
-      }
-      return false;
-    }
-
-    else {
-      window.clearTimeout(t)
-      t = window.setTimeout( function() {
-        $("#search-form").submit()
-      }, 200);
+  new domainr.SearchBox({
+    observe: _in,
+    clientId: '{your-clientid-goes-here}',
+    renderTo: _results,
+    limit: 10,
+    onSelect: function(result) {
+      var url = 'https://domainr.com/' + result.domain + '?q=' + _in.value;
+      createChromeTab(url);
     }
   });
 
-  $("#results-list li a").live('click', function(ev) {
-    var url = $(this).attr('href') + "/with/" + $(this).text()
-    createChromeTab(url)
-  });
-
-})
+})();
