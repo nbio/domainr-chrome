222cf22feea027342b45fdea7e74a33e2ea37473
Ian Gilman <ian@iangilman.com>
2015-09-21 10:45:34 -0700

    First version of Gulp support

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-Chrome extension that utilizes the Domainr API. Allows you to search & purchase domains super quickly. 
+Chrome extension that utilizes the Domainr API. Allows you to search & purchase domains super quickly.
 
-http://bit.ly/domainr-chrome 
+http://bit.ly/domainr-chrome
+
+## Development
+
+You'll need to have [Node](https://nodejs.org/) and [Gulp](http://gulpjs.com/) installed.
+
+* `npm install` to install dependencies
+* `gulp build` to build the code.
+* `gulp watch` to watch the code, building on changes.
+* `gulp` builds + watch, and runs the demo in a webpage.
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+/* globals process, console */
+'use strict';
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var replace = require('gulp-replace');
+var httpServer = require('http-server');
+var openBrowser = require('opener');
+var minifyCss = require('gulp-minify-css');
+
+var dest = './build/';
+
+// ----------
+gulp.task('serve', ['watch'], function() {
+  var port = process.env.PORT || 3102;
+  var server = httpServer.createServer();
+  return server.listen(port, 'localhost', function () {
+    console.log('Server listening at http://localhost:' + port);
+    openBrowser('http://localhost:' + port + '/domainr.html');
+  });
+});
+
+// ----------
+gulp.task('watch', ['build'], function() {
+  return gulp.watch(['js/*.*', 'stylesheets/*.*'], ['build']);
+});
+
+// ----------
+gulp.task('js', function() {
+  var scripts = [
+    './node_modules/domainr-search-box/dist/domainr-search-box.min.js',
+    './js/script.js'
+  ];
+
+  return gulp.src(scripts)
+    .pipe(uglify())
+    .pipe(concat('script.js'))
+    .pipe(replace(/{your-clientid-goes-here}/g, 'domainr-chrome'))
+    .pipe(gulp.dest(dest));
+});
+
+// ----------
+gulp.task('css', function() {
+  var styles = [
+    './node_modules/domainr-search-box/dist/domainr-search-box.css',
+    './stylesheets/styles.css'
+  ];
+
+  return gulp.src(styles)
+    .pipe(minifyCss())
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest(dest));
+});
+
+// ----------
+gulp.task('build', ['js', 'css']);
+
+// ----------
+gulp.task('default', ['serve']);
